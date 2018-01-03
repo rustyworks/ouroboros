d08a5121bb9b49fc78df2eac3741437a04143bc1
tristanto <tristanto.kurniawan@gmail.com>
2018-01-03 23:54:49 +0700

    Add log while checking spam and fix the hour :sob:

--- app/checker.py
+++ app/checker.py
@@ -5,23 +5,32 @@ class Checker(object):
 
     def is_spam(self, text):
         clean_text = self._clean_up_text(text)
-        return (self._is_phone_num(clean_text) or
+        return (self._is_banned_words(clean_text) or
                 self._is_link(clean_text) or
-                self._is_banned_words(clean_text))
+                self._is_phone_num(clean_text))
 
     def _is_phone_num(self, text):
         pattern = '\(?([0-9]{3,4})\)?[-.●]?([0-9]{3,4})[-.●]?([0-9]{3,4})'
-        return re.search(pattern, text)
+        phone_num = re.search(pattern, text)
+        if phone_num:
+            print('Phone Num Detected')
+        return phone_num
 
     def _is_link(self, text):
         pattern = '[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)'
-        return re.search(pattern, text)
+        link = re.search(pattern, text)
+        if link:
+            print('Link detected')
+        return link
 
     def _is_banned_words(self, text):
-        pattern = '(jual|beli|www)'
-        return re.search(pattern, text)
+        pattern = '(jual|beli|www|thor)'
+        banned_words = re.search(pattern, text)
+        if banned_words:
+            print('Banned words')
+        return banned_words
 
     def _clean_up_text(self, text):
         pattern = '\W+'
-        return re.sub(pattern, '', text)
+        return re.sub(pattern, '', text).lower()
 
--- app/utils.py
+++ app/utils.py
@@ -3,7 +3,7 @@ import os
 
 def seconds_to_time(seconds):
     seconds = int(seconds)
-    return '{hours:02d}:{minutes:02d}:{seconds:02d}'.format(hours=(seconds // 60), minutes=(seconds // 60 % 60), seconds=(seconds % 60))
+    return '{hours:02d}:{minutes:02d}:{seconds:02d}'.format(hours=(seconds // 3600), minutes=(seconds // 60 % 60), seconds=(seconds % 60))
 
 def delete_file(file_path):
     os.remove(file_path)
